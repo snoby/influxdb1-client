ce670668a5f499289c944a76eedc9d394cc8e817
iotapi322 <matt.snoby@icloud.com>
2019-08-22 13:58:14 -0400

    blew away all references to influxdata

--- v2/client.go
+++ v2/client.go
@@ -1,5 +1,4 @@
 // Package client (v2) is the current official Go client for InfluxDB.
-//package client // import "github.com/influxdata/influxdb1-client/v2"
 package client // import "github.com/snoby/influxdb1-client/v2"
 
 import (
